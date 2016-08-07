e2354fa37041e6eb9695b47d2896c8042801dfe9
kareem1234 <kareem.lewis79@gmail.com>
2016-08-06 20:30:32 -0400

    medication panel with items sorted by time

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,13 +5,25 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
     var start = moment().format('MM/DD/YYYY'),
         end = moment().add(1, 'day').format('MM/DD/YYYY');
 
-    $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
-        $scope.meds = meds.data;
-    });
+    this.getMeds = ()=>{
+    	$http.get('/api/medications?start=' + start + '&end=' + end).then( (meds)=> {
+	    	console.log(meds);
+	    	meds.data.forEach((med)=>{
+	    		med.timeInt = moment(med.time).unix();
+	    		med.time = moment(med.time).calendar();
+	    	})
+	        this.meds = meds.data;
+	        this.meds.sort((a,b)=>{
+	        	console.log(a.timeInt);
+	        	return a.timeInt - b.timeInt;
+	        });
+    	});
+    };
 
     $window.setInterval(function () {
         $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
         $scope.$apply();
     }, 1000);
+    this.getMeds();
 
 });
